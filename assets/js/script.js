14b4cb334ba601bf06289cad3a6e81e917573d31
Spencer Pichette <ssptuningperformance@gmail.com>
2022-06-26 19:30:24 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,16 +74,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
   update: function () {
     var tempArr = [];
@@ -117,13 +119,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     // remove dragged element from the dom
-    ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function (event, ui) {
-    console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
 });
 
@@ -146,7 +148,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -254,6 +256,12 @@ $("#remove-tasks").on("click", function () {
   console.log(tasks);
   saveTasks();
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 30);
 
 // load tasks for the first time
 loadTasks();
